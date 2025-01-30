4b67ec661956e54ec3bd88f40ce7a9a9c6d5d683
Samuel Adams <sama29571@gmail.com>
2025-01-30 19:07:40 +0000

    add class one solutions

---
+++ class 1/convert_temperature.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def convertTemperature(self, celsius):
+        """
+        :type celsius: float
+        :rtype: List[float]
+        """
+        return [celsius+273.15, celsius * 1.80 + 32.00]
+
+        
