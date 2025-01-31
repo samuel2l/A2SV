66fc4ca8b41528172fdf9fa6c0a19987d27f1ef2
Samuel Adams <sama29571@gmail.com>
2025-01-31 18:13:58 +0000

    add fizzbuzz

---
+++ fiz_buzz.py
@@ -0,0 +1,17 @@
+
+# num=int(input())
+
+for i in range(1,101):
+    if i % 3 == 0 and i % 5 == 0:
+        print(i," fizzbuzz")
+    elif i%3==0:
+        print(i," fizz")
+    elif i%5==0:
+        print(i," buzz")
+    else:
+        print(i," none") 
+
+
+
+
+
