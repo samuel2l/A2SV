f6f524f56cc24627cba64c2730d0ee832439a3de
Samuel Adams <sama29571@gmail.com>
2025-04-12 15:49:07 +0000

    coins

--- E_Coins.py
+++ E_Coins.py
@@ -1,3 +1,18 @@
 t=int(input())
 for _ in range(t):
-    
+    n,k=map(int,input().split())
+    solved=False
+
+    if n%2==0:
+        solved=True
+    #try smallest value of k. If the remainder is even then 2x can add and hence is possible
+    if (n-k)%2==0:
+        solved=True
+                
+    if solved:
+        print("YES")
+    else:
+        print("NO")
+
+
+
